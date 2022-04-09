f7641f98ab86fc4d5200e2d9f613c7dd1c965c95
ksparkbr <ksparkbr@gmail.com>
2022-04-09 17:00:05 +0900

    config 수정 (cors 재설정)

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     port: process.env.APP_PORT,
     cors: {
-        origin: [process.env.FRONT_ADDR1, process.env.FRONT_ADDR2],
+        origin: [process.env.FRONT_ADDR1, process.env.FRONT_ADDR2, process.env.FRONT_ADDR3, process.env.FRONT_ADDR4],
         credentials: true,
     },
 }
